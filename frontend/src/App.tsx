2aea39e9838ba2d957acf8ee726b68f4f6827655
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2026-03-16 23:07:46 -0300

    feat: integrate authentication and routing into application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import AppRoutes from "./app/router/routes"
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
-
   return (
-    <div>
-      Olá, mundo!
-    </div>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   )
 }
 
