0e91b7f1e96ec4167828c9db5c28a18156f4d764
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2026-03-18 12:06:18 -0300

    feat: add auth layout structure

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -2,16 +2,15 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#f5f5f5"
-      }}
-    >
-      <Outlet />
+    <div className="h-[100vh] w-full flex flex-col items-center justify-center">
+      <div className="w-full h-[95vh] flex justify-center items-center">
+        <Outlet />
+      </div>
+      <div>
+        <p className="text-gray-500 text-xs">
+          @2026 PayManager Systems. All rights reserved.
+        </p>
+      </div>
     </div>
   )
 }
