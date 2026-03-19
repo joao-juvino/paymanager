32f0382a8131bec43624a01c89b31e64cabd7f5a
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2026-03-19 16:38:00 -0300

    fix(users): findById returns only id, name, email and createdAt

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -8,11 +8,6 @@ export class UsersService {
   async findById(id: number) {
     return this.prisma.user.findUnique({
       where: { id },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-      },
     });
   }
 
