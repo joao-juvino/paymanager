7c1a4ce7c3d4819505a7958e8ba246b6a40abc27
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2026-03-20 16:35:48 -0300

    feat(common): create reusable PageHeader component

---
+++ frontend/src/common/components/PageHeader.tsx
@@ -0,0 +1,17 @@
+
+export default function PageHeader({ title, description, action }: {
+    title: string;
+    description: string;
+    action?: React.ReactNode;
+}) {
+    return (
+        <header className="mb-10 flex justify-between">
+            <div>
+                <h1 className="mb-2 text-3xl font-semibold font-heading">{title}</h1>
+                <p className="text-gray-600">{description}</p>
+            </div>
+            {action}
+        </header>
+    );
+}
+
