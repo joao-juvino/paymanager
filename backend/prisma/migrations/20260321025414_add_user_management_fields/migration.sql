56179d5367009f9e066d2e8eb50048684e815e3a
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2026-03-21 01:05:35 -0300

    feat(prisma): add user management fields and relations

---
+++ backend/prisma/migrations/20260321025414_add_user_management_fields/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true,
+    ADD COLUMN `lastActivityAt` DATETIME(3) NULL,
+    ADD COLUMN `username` VARCHAR(191) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_username_key` ON `User`(`username`);
